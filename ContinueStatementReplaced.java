0b03f0de17fda784596baa960e38d4a62a4f2c67
Oluwanishola227639 <barnabas.oretan@gmail.com>
2020-12-24 10:58:00 +0100

    chapter 5 Exercises

--- ContinueStatementReplaced.java
+++ ContinueStatementReplaced.java
@@ -8,14 +8,14 @@ public class ContinueStatementReplaced {
 //    public class ContinueTest
 //    {
     public static void main(String[] args) {
-        boolean condition = false;
-        for (int count = 1; ((count <= 10) == !condition); count++) {
-                if (count == 4)
 
-                    condition=true;
-                    //continue; // skip remaining code in loop body if count is 5
+        for (int count = 1; count <= 10; count++) {
+            if (count != 5) {
+                //continue; // skip remaining code in loop body if count is 5
                 System.out.printf("%d ", count);
             }
+        }
+
             System.out.printf("%nUsed continue to skip printing 5%n");
         }
     } // end class ContinueTest
